1704e9891abcdfea1ba0460513c258df4d03d329
ezzat balah <ezzat.ahamed@icloud.com>
2025-10-11 16:54:51 +0300

    recommend cubite

---
+++ lib/features/home/presentation/manger/FetchRecommend/fetch_recommend_cubit.dart
@@ -0,0 +1,24 @@
+// ignore: depend_on_referenced_packages
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:movie_app/features/home/data/model/recomended/recommended_model.dart';
+import 'package:movie_app/features/home/data/repo/home_repo_impl.dart';
+
+part 'fetch_recommend_state.dart';
+
+class FetchRecommendCubit extends Cubit<FetchRecommendState> {
+  FetchRecommendCubit(this.homeRepoImpl) : super(FetchRecommendInitial());
+  final HomeRepoImpl homeRepoImpl;
+  Future<void> fetchNewRecommend() async {
+    emit(FetchRecommendLoadind());
+    var result = await homeRepoImpl.fetchRecommendedMovies();
+    result.fold(
+      (failure) {
+        emit(FetchRecommendFailuer(failure.errorMassege));
+      },
+      (recommend) {
+        emit(FetchRecommendSuccess(recommend));
+      },
+    );
+  }
+}
---
+++ lib/features/home/presentation/manger/FetchRecommend/fetch_recommend_state.dart
@@ -0,0 +1,22 @@
+part of 'fetch_recommend_cubit.dart';
+
+sealed class FetchRecommendState extends Equatable {
+  const FetchRecommendState();
+
+  @override
+  List<Object> get props => [];
+}
+
+final class FetchRecommendInitial extends FetchRecommendState {}
+
+final class FetchRecommendLoadind extends FetchRecommendState {}
+
+final class FetchRecommendFailuer extends FetchRecommendState {
+  final String errorMassege;
+  const FetchRecommendFailuer(this.errorMassege);
+}
+
+final class FetchRecommendSuccess extends FetchRecommendState {
+  final List<RecommendedModel> recommend;
+  const FetchRecommendSuccess(this.recommend);
+}
